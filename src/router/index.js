dc655e155535ba382be3c15df9ee010377d0ff9f
finley <FinleyLu@trevi.cc>
2020-03-29 22:57:51 +0800

    Preventing router going before vuex data synchronous.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import routes from "./routes";
+import store from '@/plugins/vuex';
+
 Vue.use(VueRouter);
 
 // configure router
@@ -8,5 +10,19 @@ const router = new VueRouter({
   routes, // short for routes: routes
   linkActiveClass: "active"
 });
+
+/**
+ * https://github.com/championswimmer/vuex-persist#how-to-know-when-async-store-has-been-replaced
+ */
+const waitForStorageToBeReady = async (to, from, next) => {
+  /**
+   * if applying localforage in vuex-presist in different ways(LOCALSTORAGE & INDEXEDDB)
+   * this promise would be resolved while the first vuex synced.
+   */
+  await store.restored
+  next()
+}
+router.beforeEach(waitForStorageToBeReady)
+
 
 export default router;
