fe8b274d0d6043879d157c6a3953b5424a803f6a
finley <FinleyLu@trevi.cc>
2020-03-29 12:39:43 +0800

    Add vuex presist for sync profile in localStorage.

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,24 @@ import Vue from "vue";
 import Vuex from "vuex";
 import profile from "./modules/profile";
 import createLogger from "vuex/dist/logger";
+import VuexPersist from 'vuex-persist';
 
 Vue.use(Vuex);
 
 const logger = createLogger({
   collapsed: false // auto-expand logged mutations
 });
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex', // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  // Function that passes the state and returns the state with only the objects you want to store.
+  reducer: state => ({
+    profile: state.profile,
+  }),
+  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+  // filter: mutation => (true)
+})
 
 const store = new Vuex.Store({
   /**
@@ -20,6 +32,7 @@ const store = new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
   plugins: [
     process.env.NODE_ENV !== "production" && logger,
+    vuexLocalStorage.plugin,
   ]
 });
 
